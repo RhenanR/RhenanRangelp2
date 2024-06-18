ed5ed0cce36ebaeec251782a9dc2d63a025d0ffe
RhenanR <166552429+RhenanR@users.noreply.github.com>
2024-06-18 16:24:03 -0300

    Add files via upload

---
+++ sequencia fibonacci.py
@@ -0,0 +1,14 @@
+qnt=int(input("quantos números pertencente a sequencia você quer? "))
+conta=3
+f1=int(1)
+f2=int(1)
+print (f1)
+if qnt>1:
+    print(f2)
+fn=(f1+f2)
+while (conta<=qnt):
+    fn=(f1+f2)
+    print (fn)
+    f1=int(f2)
+    f2=int(fn)
+    conta=conta+1
