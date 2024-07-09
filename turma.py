9ea45c49ada78d822890ae8c848c16afee0e860c
RhenanR <166552429+RhenanR@users.noreply.github.com>
2024-07-09 15:45:13 -0300

    Delete turma.py

--- turma.py
+++
@@ -1,8 +0,0 @@
-turma=[]
-for nome in range (1,30):
-    nome=str(input("um nome de um aluno: "))
-    turma.append(nome)
-    if ((nome)=="fim"):
-        turma.pop(-1)
-        break
-print (turma)
